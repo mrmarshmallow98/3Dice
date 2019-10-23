6287d7727310ea290a58e761ca335080322b33c1
mrmarshmallow98 <ahjie981127@gmail.com>
2019-10-23 09:42:23 +0800

    Latest 3 dice roller

--- app/src/main/java/com/example/a3dice/MainActivity.kt
+++ app/src/main/java/com/example/a3dice/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.a3dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.a3dice.R
@@ -11,33 +12,44 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage1 : ImageView
+    lateinit var diceImage2 : ImageView
+    lateinit var diceImage3 : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage1 = findViewById(R.id.dice_image1)
+        diceImage2 = findViewById(R.id.dice_image2)
+        diceImage3 = findViewById(R.id.dice_image3)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-    }
 
+    }
 
     private fun rollDice() {
 
-        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        diceImage1.setImageResource(randomInt())
+        diceImage2.setImageResource(randomInt())
+        diceImage3.setImageResource(randomInt())
+    }
 
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        val resultText3: TextView = findViewById(R.id.result_text3)
-        resultText1.text = "Dice Rolled!"
+    private fun randomInt() : Int {
 
         val randomInt = Random().nextInt(6) + 1
-        resultText1.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val randomInt1 = Random().nextInt(6) + 1
-        resultText2.text = randomInt1.toString()
-
-        val randomInt2 = Random().nextInt(6) + 1
-        resultText3.text = randomInt2.toString()
+        return drawableResource
     }
 
 
